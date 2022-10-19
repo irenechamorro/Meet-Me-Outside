05fe1c54f1354b887f2685af81359834c9fd8155
molinar1999 <matthew.a.molinar@gmail.com>
2022-10-19 17:57:46 -0500

    added error animation to signup

--- Meet Me Outside/SignupViewController.swift
+++ Meet Me Outside/SignupViewController.swift
@@ -27,9 +27,18 @@ class SignupViewController: UIViewController {
     
 
     @IBAction func signupButtonPressed(_ sender: Any) {
+        // Creating an animation to shake the textfield if there is an error.
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        animation.isAdditive = true
+        
         Auth.auth().createUser(withEmail: signupEmailField.text!, password: signupPasswordField.text!) {
             authResult, error in
             if let error = error as NSError? {
+                self.signupPasswordField.layer.add(animation, forKey: "shake")
                 print("\(error.localizedDescription)")
             } else {
                 print("successfully created user")
